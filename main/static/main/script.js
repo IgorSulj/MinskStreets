5e4e37b2501e32f5ed7e8af5288f67f76a6df3e9
Artemy-sweden <61091758+Artemy-sweden@users.noreply.github.com>
2021-11-15 20:41:02 +0300

    Frontend-fixedZoomInOutSystem

--- main/static/main/script.js
+++ main/static/main/script.js
@@ -1,6 +1,8 @@
 var isImageZoomed = false;
+var currentId;
 
 function zoom(id) {
+  currentId = id;
   if (!isImageZoomed) {
     document.getElementById(id).classList.toggle("zoomed");
     document.querySelector(".shadowbox").style.display = "block";
@@ -10,4 +12,10 @@ function zoom(id) {
     document.querySelector(".shadowbox").style.display = "none";
     isImageZoomed = false;
   }
+}
+
+function zoomOutByShadow() {
+  document.querySelector(".shadowbox").style.display = "none";
+  document.getElementById(currentId).classList.toggle("zoomed");
+  isImageZoomed = false;
 }
