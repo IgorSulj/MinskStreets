cdb64183db33d6b16063c69e500af067b58e91d6
IgorSulj <igorsuljin@gmail.com>
2021-11-20 20:06:28 +0000

    Improved security

--- MinskStreets/settings.py
+++ MinskStreets/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$k2_1!71h1qhr6b5-^40-tzi@s&41)r&jst6riaa-0oa%t(k8='
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-$k2_1!71h1qhr6b5-^40-tzi@s&41)r&jst6riaa-0oa%t(k8=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEBUG' not in os.environ
 
-ALLOWED_HOSTS = ['minskstreets.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
 
 
 # Application definition
