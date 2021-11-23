b119ba558c10c712a86f484ee5122ee380fb4878
IgorSulj <igorsuljin@gmail.com>
2021-11-23 09:44:49 +0000

    Map Rework

--- main/static/main/index_map.js
+++ main/static/main/index_map.js
@@ -1,11 +1,163 @@
+let map_data = [{
+    "geometry": {"type": "Point", "coordinates": [27.574749, 53.929134]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-maksima-bogdanovicha\">улица Максима Богдановича</a>",
+        "iconContent": "1",
+        "marker": "islands#redCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.444682, 53.924373]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-alesya-garuna\">улица Алеся Гаруна</a>",
+        "iconContent": "2",
+        "marker": "islands#darkOrangeCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.578414, 53.839484]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-yanki-luchiny\">улица Янки Лучины</a>",
+        "iconContent": "3",
+        "marker": "islands#orangeCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.595311, 53.910596]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-platonova\">улица Платонова</a>",
+        "iconContent": "4",
+        "marker": "islands#yellowCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.574991, 53.950202]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-stefanii-stanyuty\">улица Стефании Станюты</a>",
+        "iconContent": "5",
+        "marker": "islands#pinkCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.557546, 53.910803]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-arhitektora-zaborskogo\">улица Архитектора Заборского</a>",
+        "iconContent": "6",
+        "marker": "islands#nightCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.514346, 53.843915]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-korzhenevskogo\">улица Корженевского</a>",
+        "iconContent": "7",
+        "marker": "islands#darkBlueCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.574929, 53.870989]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-syomenova\">улица Семёнова</a>",
+        "iconContent": "8",
+        "marker": "islands#blueCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.520823, 53.889783]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-hmelevskogo\">улица Хмелевского</a>",
+        "iconContent": "9",
+        "marker": "islands#lightBlueCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.481647, 53.901053]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-belskogo\">улица Бельского</a>",
+        "iconContent": "10",
+        "marker": "islands#blackCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.627992, 53.932712]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-kedyshko\">улица Кедышко</a>",
+        "iconContent": "11",
+        "marker": "islands#oliveCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.694153, 53.93954]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-nikiforova\">улица Никифорова</a>",
+        "iconContent": "12",
+        "marker": "islands#grayCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.593533, 53.940043]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-galo\">улица Гало</a>",
+        "iconContent": "13",
+        "marker": "islands#darkGreenCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.556118, 53.877417]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-levkova\">улица Левкова</a>",
+        "iconContent": "14",
+        "marker": "islands#greenCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.582088, 53.847411]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-chizhevskih\">улица Чижевских</a>",
+        "iconContent": "15",
+        "marker": "islands#violetCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.516403, 53.896119]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-voloha\">улица Волоха</a>",
+        "iconContent": "16",
+        "marker": "islands#brownCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.547575, 53.931048]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-very-sluckoj\">улица Веры Слуцкой</a>",
+        "iconContent": "17",
+        "marker": "islands#orangeCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.511274, 53.880666]},
+    "properties": {
+        "description": "<a href=\"https://minskstreets.pythonanywhere.com/streets/ulica-shatko\">улица Шатько</a>",
+        "iconContent": "18",
+        "marker": "islands#darkOrangeCircleIcon"
+    }
+}, {
+    "geometry": {"type": "Point", "coordinates": [27.579932, 53.896803]},
+    "properties": {
+        "description": "<a href=\"/streets/ulica-pulihova\">улица Пулихова</a>",
+        "iconContent": "19",
+        "marker": "islands#oliveCircleIcon"
+    }
+}]
+
+// let color_to_icon = {
+//     "#ed4543": "islands#redCircleIcon",
+//     "#e6761b": "islands#darkOrangeCircleIcon",
+//     "#ff931e": "islands#orangeCircleIcon",
+//     "#ffd21e": "islands#yellowCircleIcon",
+//     "#f371d1": "islands#pinkCircleIcon",
+//     "#0e4779": "islands#nightCircleIcon",
+//     "#177bc9": "islands#darkBlueCircleIcon",
+//     "#1e98ff": "islands#blueCircleIcon",
+//     "#82cdff": "islands#lightBlueCircleIcon",
+//     "#595959": "islands#blackCircleIcon",
+//     "#97a100": "islands#oliveCircleIcon",
+//     "#b3b3b3": "islands#grayCircleIcon",
+//    
+// }
 
 ymaps.ready(() => {
     let map = new ymaps.Map("map", {
         center: [53.902284, 27.561831],
-        zoom: 10
+        zoom: 11
     })
-    let data = ymaps.geoXml.load("https://minskstreets.pythonanywhere.com/static/main/MinskStreets.kml")
-    data.then(result => {
-        map.geoObjects.add(result.geoObjects)
+    map.controls.add(new ymaps.control.ZoomControl())
+    map_data.forEach(point => {
+        point.geometry.coordinates.reverse()
+        let geoObj = new ymaps.GeoObject(point, {preset: point.properties.marker})
+        map.geoObjects.add(geoObj)
     })
 })
